c9d74d4a1a62898c789804395d4310a3b4a3d948
zcx <1287341777@qq.com>
2020-12-04 15:18:14 +0800

    add

--- cocosCreatorPro/shaderProject/assets/Pro/waterfall/waterfall.ts
+++ cocosCreatorPro/shaderProject/assets/Pro/waterfall/waterfall.ts
@@ -15,14 +15,23 @@ export default class NewClass extends cc.Component {
     @property(cc.Sprite)
     spWaterShow: cc.Sprite = null;
 
+    @property(cc.Node)
+    ndWater: cc.Node = null;
+
 
     onLoad () {
+        this.spWaterShow.node.scaleY = -1;//Y轴翻转
         const texture = new cc.RenderTexture();
         texture.initWithSize(this.spWaterShow.node.width, this.spWaterShow.node.height);
         const spriteFrame = new cc.SpriteFrame();
         spriteFrame.setTexture(texture);
         this.cameraWater.targetTexture = texture;
         this.spWaterShow.spriteFrame = spriteFrame;
+
+        this.ndWater.on(cc.Node.EventType.TOUCH_MOVE,(event)=>{
+            let position = event.touch.getLocation();
+            this.ndWater.setPosition(this.ndWater.parent.convertToNodeSpaceAR(position));
+        })
     }
 
     // update (dt) {}
